77a356998dde9b4393218e5540154edb36160331
Bavisetti Siva Avinash <48872557+Bavisettisivaavinash@users.noreply.github.com>
2024-09-06 23:17:01 +0530

    Pull practice MyFirstJava.java

--- MyFirstJava.java
+++ MyFirstJava.java
@@ -1,5 +1,6 @@
 class Main{
     public static void main(String[] args){
         System.out.println("Hello this is avinash");
+        System.out.println("Hello this is a change made by your team mate!!");
     }
 }
